67981eac2f424080d044e8a4e52733eca5bc408b
ExproVic <viktor19970429@gmail.com>
2023-04-08 20:17:12 +0200

    update Navbar v1.1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-    return <nav className={classes.nav}>
-        <div className={classes.item}><a href='/profile'>Profile</a></div>
-        <div className={classes.item}><a href='/dialogs'>Messages</a></div>
-        <div className={classes.item}><a href='/news'>News</a></div>
-        <div className={classes.item}><a href='/setting'>Setting</a></div>
-      
+  return <nav className={classes.nav}>
+    <div>
+
+      <NavLink to='/profile' className={navData => navData.isActive ? classes.active : classes.item}>Profile</NavLink>
+    </div>
+    <div className={classes.item}>
+      <NavLink to='/dialogs' className={navData => navData.isActive ? classes.active : classes.item}>Messages</NavLink>
+    </div>
+    <div className={classes.item}>
+      <NavLink to='/news' className={navData => navData.isActive ? classes.active : classes.item}>News</NavLink>
+    </div>
+    <div className={classes.item}>
+      <NavLink to='/setting' className={navData => navData.isActive ? classes.active : classes.item}>Setting</NavLink>
+    </div>
+
   </nav>
-      
-    
+
+
 }
 export default Navbar
