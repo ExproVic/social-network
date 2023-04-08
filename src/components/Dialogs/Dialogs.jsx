e2eacab3bc00cf2d3bf5d5a021fb02b378930e19
ExproVic <viktor19970429@gmail.com>
2023-04-08 22:57:31 +0200

    update dialogs 1.1

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import classes from'./Dialogs.module.css';
+import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-      <div className={classes.item}>Dialogs</div>
-    )
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={classes.dialog + '' + classes.active}>Victor</div>
+        <div className={classes.dialog}>Malia</div>
+        <div className={classes.dialog}>Taya</div>
+        <div className={classes.dialog}>Antonia</div>
+        <div className={classes.dialog}>Melisa</div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hello everybody</div>
+        <div className={classes.message}>yo sup</div>
+        <div className={classes.message}> okay</div>
+      </div>
+    </div>
+  )
 }
 export default Dialogs
