cd38e495f97461e51330bc3d55352c0e0fd71afd
ExproVic <viktor19970429@gmail.com>
2023-04-22 23:18:22 +0200

    prepare

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
+import { addPostActionCreator, updateNewPostTextActionCreator } from "../../../redux/state";
 
 const MyPosts = (props) => {
     let postsElement =
         props.posts.map(p => <Post message={p.message} like={p.like} />);
     let newPostElement = React.createRef();
     let addPost = () => {
-        props.dispatch({ type: 'ADD-POST' });
+        props.dispatch(addPostActionCreator());
     }
+
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = { type: 'UPDATE - NEW - POST - TEXT', newText: text };
+        let action = updateNewPostTextActionCreator(text);
         props.dispatch(action);
     }
+
 
     return (
         <div className={classes.postsBlock}>
